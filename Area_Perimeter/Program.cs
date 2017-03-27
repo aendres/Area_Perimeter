cb5bc6a4ffeadebc8064e43887e7d5a0ac0c23b8
aendres <aendres19@gmail.com>
2017-03-27 16:39:49 -0400

    Add program that gives area and perimeter of the numbers inputed

--- Area_Perimeter/Program.cs
+++ Area_Perimeter/Program.cs
@@ -10,6 +10,35 @@ namespace Area_Perimeter
     {
         static void Main(string[] args)
         {
+            string result;
+            do
+            {
+                Console.WriteLine("Welcome to Grand Circus' Room Detail Generator");
+
+                string width;
+                string length;
+
+                Console.WriteLine("Enter Length: ");
+                length = Console.ReadLine();
+                float Length = float.Parse(length);
+
+                Console.WriteLine("Enter Width: ");
+                width = Console.ReadLine();
+                float Width = float.Parse(width);
+
+                float area = Length * Width;
+                Console.WriteLine("Area:" + area);
+
+                float perimeter = ((2 * Length) + (2 * Width));
+                Console.WriteLine("Perimeter:" + perimeter);
+
+                Console.WriteLine("Continue? (yes/no):");
+                result = Console.ReadLine();
+            }
+            while (result == "yes");
+            {
+                Console.WriteLine("Have a good day!");
+            }
         }
     }
 }
